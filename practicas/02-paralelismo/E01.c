d76a1d29413bae5c88406a49cb12e2cbc12d208a
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-11-14 11:00:43 -0500

    practice 02 - temperature with paralelism

--- practicas/02-paralelismo/E01.c
+++ practicas/02-paralelismo/E01.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <omp.h>
 
-void fill_temp(float arr[7][24], int rows, int cols)
+void fill_temp(double arr[7][24], int rows, int cols)
 {
   srand48(time(NULL));
 
@@ -12,7 +12,7 @@ void fill_temp(float arr[7][24], int rows, int cols)
   {
     for (size_t j = 0; j < cols; j++)
     {
-      arr[i][j] = drand48() * (50.0 - 5.0) + 5.0;
+      arr[i][j] = (double)drand48() * (50.0 - 5.0) + 5.0;
     }
   }
 }
@@ -23,32 +23,36 @@ int main()
 
   bool days_over30[7] = {false, false, false, false, false, false, false};
 
-  float temp[7][24];
+  double temp[7][24];
   int days = sizeof(temp) / sizeof(temp[0]);
   int hours = sizeof(temp[0]) / sizeof(temp[0][0]);
 
   fill_temp(temp, days, hours);
 
   // Variables for the excercise
-  float min_week_temp = temp[0][0], max_week_temp = temp[0][0];
-  float min_day_temp[days], max_day_temp[days];
-  float mean_week_temp;
-  float mean_day_temp[days];
+  double min_week_temp = temp[0][0], max_week_temp = temp[0][0];
+  double min_day_temp[days], max_day_temp[days];
+  double mean_week_temp = 0;
+  double mean_day_temp[days];
 
-  omp_set_num_threads(5);
+  omp_set_num_threads(4);
 
-#pragma omp parallel shared(temp)
+#pragma omp parallel shared(temp, min_day_temp, max_day_temp, mean_week_temp, mean_day_temp)
   {
     int threads = omp_get_num_threads();
     int thread = omp_get_thread_num();
 
     for (size_t i = 0; i < days; i++)
     {
+      double aux_min_temp = temp[i][0];
+      double aux_max_temp = temp[i][0];
+      double aux_mean_temp = 0;
+
       for (size_t j = 0; j < hours; j++)
       {
         switch (thread)
         {
-        case 1:
+        case 0:
           /* Get Max and min temp week */
           if (temp[i][j] < min_week_temp)
             min_week_temp = temp[i][j];
@@ -57,24 +61,59 @@ int main()
             max_week_temp = temp[i][j];
 
           break;
-        case 2:
+        case 1:
           /* Get max and min temp day */
+          if (temp[i][j] < aux_min_temp)
+          {
+            aux_min_temp = temp[i][j];
+            min_day_temp[i] = aux_min_temp;
+          }
+
+          if (temp[i][j] > aux_max_temp)
+          {
+            aux_max_temp = temp[i][j];
+            max_day_temp[i] = aux_max_temp;
+          }
+
+          break;
+        case 2:
+          /* mean week temp */
+          mean_week_temp += temp[i][j];
+
           break;
         case 3:
-          /* mean week temp */
-          break;
-        case 4:
           /* mean temp day */
-          break;
-        case 5:
-          /* days when the temp is over 30 */
+          mean_day_temp[i] += temp[i][j];
           break;
         }
       }
     }
   }
 
+  mean_week_temp /= days * hours;
+
+  for (size_t i = 0; i < days; i++)
+  {
+    if ((mean_day_temp[i] /= hours) > 30)
+      days_over30[i] = true;
+  }
+
+  printf("Max and min temp per day:\n");
+  for (size_t i = 0; i < days; i++)
+  {
+    printf("* %s:\n\tmin: %.2f°c, max: %.2f°c, mean: %.2f°c\n", week_days[i], min_day_temp[i], max_day_temp[i], mean_day_temp[i]);
+  }
+
   printf("\n* All the week:\n\tmin: %.2f°c, max: %.2f°c, mean: %.2f°c\n", min_week_temp, max_week_temp, mean_week_temp);
+
+  printf("\nDays when the temp was over 30°c: \n");
+  for (size_t i = 0; i < days; i++)
+  {
+    if (days_over30[i])
+    {
+      printf("\t* %s\n", week_days[i]);
+    }
+  }
 
   return 0;
 }
