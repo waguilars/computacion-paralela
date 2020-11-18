da56ac43f05c40700720cebd61049267c67f74b3
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-11-18 18:26:10 -0500

    homework 2 ex 8 done

---
+++ deberes/02-paralelismo/P08.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+int *split_in_digits(int number, int digits)
+{
+  //Get number of digits
+
+  int *digits_array = (int *)malloc(sizeof(int) * digits);
+
+  for (size_t i = 0; i < digits; i++)
+  {
+    *(digits_array + i) = number % 10;
+    number = number / 10;
+  }
+
+  return digits_array;
+}
+
+int main()
+{
+  int n = 3,   // digits number
+      x = 153, // number to eval
+      sum = 0;
+  int *digits = split_in_digits(x, n);
+
+  omp_set_num_threads(4);
+#pragma omp parallel for shared(digits, n) reduction(+ \
+                                                     : sum)
+  for (size_t i = 0; i < n; i++)
+  {
+    int tid = omp_get_thread_num();
+    int cube = pow(digits[tid], 3);
+    printf("Thread %d: %d^3 = %d\n", tid, digits[tid], cube);
+    sum += cube;
+  }
+
+  printf("Suma: %d\n", sum);
+
+  if (x == sum)
+    printf("El valor %d es cubo perfecto!\n", x);
+  else
+    printf("El valor %d no es un cubo perfecto!! :(\n", x);
+
+  return 0;
+}
